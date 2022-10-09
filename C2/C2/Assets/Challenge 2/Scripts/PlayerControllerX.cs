1ab720c9c7bffa23ed96de843ff7d8cfff4268d3
Asper Sarras <101324242@georgrebrown.ca>
2022-10-09 17:40:47 -0200

    Challange 1 and 2

---
+++ C2/C2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    private bool cd = false;
+    private float cdCap = 2;
+    private float cdCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(cd==true)
+        {
+            cdCount += Time.deltaTime;
+            if(cdCount >= cdCap)
+            {
+                cdCount = 0;
+                cd =false;
+            }
+        }
+        else
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                cd = true;
+            }
+        }
+
+        // On spacebar press, send dog
+
+    }
+}
---
+++ C2/C2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+
+    private float spawnLimitXLeft = -20;
+    private float spawnLimitXRight = 7;
+    private float spawnPosY = 30;
+
+    //private float startDelay = 1.0f;
+    private float spawnInterval;
+
+    private int rndmBall;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnInterval = Random.Range(3, 6);
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+
+    // Spawn random ball at random x position at top of play area
+    void SpawnRandomBall ()
+    {
+        rndmBall = Random.Range(0, 3);
+
+        // Generate random ball index and random spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+        // instantiate ball at random spawn location
+        Instantiate(ballPrefabs[rndmBall], spawnPos, ballPrefabs[rndmBall].transform.rotation);
+
+        spawnInterval = Random.Range(3, 6);
+
+        Invoke("SpawnRandomBall", spawnInterval);
+    }
+
+}
